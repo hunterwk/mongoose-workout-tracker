f84d0546954d13d2f4349d665d065a366aa3ff48
Hunter Kantner <hunterkantner@gmail.com>
2020-11-01 16:41:50 -0800

    process.env.mongodb add

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/dbExample", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
